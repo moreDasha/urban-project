0901d73362c4a57da68bd8cba218797ac13c9e41
moreDasha <moredasha@icloud.com>
2023-02-13 02:06:36 +0400

    feat: add js

--- js/index.js
+++ js/index.js
@@ -15,5 +15,28 @@ window.addEventListener('resize',function(){
         cardWrap.forEach((e) => {
             unwrap(e);
         })
+    } else {
+        location.reload();
     }
 });
+
+
+
+let lastScroll = 0;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+    if(scrollPosition() > lastScroll && !containHide()) {
+        //scroll down
+        header.classList.add('hide');
+    }
+    else if(scrollPosition() < lastScroll && containHide()){
+        //scroll up
+        header.classList.remove('hide');
+    }
+
+    lastScroll = scrollPosition();
+})
