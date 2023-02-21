2e9601a8c68b21a476a32ebe11119d038558f4b7
moreDasha <moredasha@icloud.com>
2023-02-21 21:41:35 +0400

    feat: add hover on cards

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-const unwrap = (element) => {
-    element.replaceWith(...element.children);
-};
-const cardWrap = document.querySelectorAll('.card-item-wrapper');
-
-if (document.documentElement.clientWidth < 750) {
-    cardWrap.forEach((e) => {
-        unwrap(e);
-    })
-}
-
-window.addEventListener('resize',function(){
-    let windoWith = document.documentElement.clientWidth;
-    if (windoWith < 750) {
-        cardWrap.forEach((e) => {
-            unwrap(e);
-        })
-    } else {
-        location.reload();
-    }
-});
-
-
 
 let lastScroll = 0;
 const header = document.querySelector('.header');
